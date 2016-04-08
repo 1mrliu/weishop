74acba3c6c686930149fb6940b1a11d8bd7b8a0c
栋 <ld815626@hotmail.com>
2016-04-08 10:53:20 +0800

    add login

--- src/com/example/weishop/tapFour.java
+++ src/com/example/weishop/tapFour.java
@@ -5,16 +5,16 @@ package com.example.weishop;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
-import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.TextView;
@@ -27,13 +27,15 @@ public class tapFour extends Fragment {
 	{
 		View view=inflater.inflate(R.layout.tab04, container, false);
 		
+		//我的订单
 		TextView tv_dingdan=(TextView)view.findViewById(R.id.dingdan);
         tv_dingdan.setText(Html.fromHtml("<u>"+"我的订单"+"</u>"));
         
+        //我的收藏
         TextView tv_shoucang=(TextView)view.findViewById(R.id.shoucang);
         tv_shoucang.setText(Html.fromHtml("<u>"+"我的收藏"+"</u>"));
-		
-		
+        
+        //个人中心
 		ListView lv=(ListView)view.findViewById(R.id.personal_center);
 		 
 		//详细界面适配器
@@ -50,6 +52,20 @@ public class tapFour extends Fragment {
 						R.layout.list_item_personal_info,new String[]{"image_personal","function_info","image_personal_icon"},
 						new int[]{R.id.image_personal,R.id.function_info,R.id.image_personal_icon});    	  
 				lv.setAdapter(mSimpleAdapter);//为ListView绑定适配器
+				lv.setOnItemClickListener(new OnItemClickListener() {
+
+					@Override
+					public void onItemClick(AdapterView<?> parent, View view,
+							int position, long id) {
+						if(position==3){
+							Intent intent=new Intent();
+							intent.setClass(getActivity(), Login.class);
+							startActivity(intent);
+							
+						}
+						
+					}
+				});
 		
 		return view;
 	}

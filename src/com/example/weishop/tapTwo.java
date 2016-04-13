79718ce9447260c6216a08d9d18e3ad09099c578
栋 <ld815626@hotmail.com>
2016-04-13 16:12:36 +0800

    vfff

--- src/com/example/weishop/tapTwo.java
+++ src/com/example/weishop/tapTwo.java
@@ -18,8 +18,11 @@ import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 import org.apache.http.HttpEntity;
@@ -32,58 +35,64 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 
 public class tapTwo extends Fragment {
-	
+
 	public String[] data = { "热卖专区", "进口精品", "国产优选", "森林干果", "礼盒整箱", "新鲜果汁" };
-	public List<Map<String, Object>> mListItem = new ArrayList<Map<String,Object>>();
-	 ListView mListView,mListView_1,lv_type;
+	public List<Map<String, Object>> mListItem = new ArrayList<Map<String, Object>>();
+	ListView mListView, mListView_1, lv_type;
+	/*
+	 * 获取商品当前数量并改变数字的大小
+	 */
+	EditText count;
 	public static final int SHOW_RESPONSE = 0;
-	
+
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		final View view = inflater.inflate(R.layout.tab02, container, false);
 		// 商品分类
-		lv_type = (ListView) view.findViewById(android.R.id.list);	
+		lv_type = (ListView) view.findViewById(android.R.id.list);
 		sendRequestWithHttpClient();
-		ArrayAdapter<String>  lv_type_adapter=new ArrayAdapter<String>(getActivity(),
-				android.R.layout.simple_list_item_1,data);
+		ArrayAdapter<String> lv_type_adapter = new ArrayAdapter<String>(
+				getActivity(), android.R.layout.simple_list_item_1, data);
 		lv_type.setAdapter(lv_type_adapter);
-		
+
 		// 商品详细情况展示
 		mListView = (ListView) view.findViewById(R.id.cont1);
 		mListView_1 = (ListView) view.findViewById(R.id.cont2);
+		count = (EditText) view.findViewById(R.id.list_buy_number);
 		return view;
-		}
+	}
+
 	//
 	private Handler handler = new Handler() {
 
-	
-		@SuppressLint("HandlerLeak") 
+		@SuppressLint("HandlerLeak")
 		public void handleMessage(Message msg) {
 			switch (msg.what) {
 			case 1:
-				
+
 				String response = (String) msg.obj;
 				parseJSONWithJSONObject(response);
 				MyAdapter adapter = new MyAdapter(getActivity());
-				
+
 				mListView.setAdapter(adapter);
 				mListView_1.setAdapter(adapter);
-				
+
 				// 设置进入选购中心显示的商品界面，修复进入界面是加载全部的信息问题
 				mListView.setVisibility(View.INVISIBLE);
 				mListView_1.setVisibility(View.INVISIBLE);
-				
+
 				// getActivity使用在适配器中适用于Fragment中
-				ArrayAdapter<String> typeAdapter = new ArrayAdapter<String>
-				(getActivity(), android.R.layout.simple_list_item_1, data);
+				ArrayAdapter<String> typeAdapter = new ArrayAdapter<String>(
+						getActivity(), android.R.layout.simple_list_item_1,
+						data);
 				lv_type.setAdapter(typeAdapter);
-				
+
 				// 分类适配器类别添加点击事件
 				lv_type.setOnItemClickListener(new OnItemClickListener() {
 					@Override
 					public void onItemClick(AdapterView<?> parent, View view,
 							int position, long id) {
-						
+
 						// 对点击位置进行判断，添加事件
 						if (data[position].equals("热卖专区")) {
 							mListView.setVisibility(View.VISIBLE);
@@ -108,26 +117,27 @@ public class tapTwo extends Fragment {
 						if (data[position].equals("新鲜果汁")) {
 							mListView.setVisibility(View.VISIBLE);
 							mListView_1.setVisibility(View.GONE);
-						}		
+						}
 					}
 				});
 				break;
 			}
 		}
 	};
+
 	private void sendRequestWithHttpClient() {
 		new Thread(new Runnable() {
 			@Override
 			public void run() {
 				try {
 					HttpClient httpClient = new DefaultHttpClient();
-					
+
 					HttpGet httpGet = new HttpGet(
 							"http://www.xxxjqt.cn/amallphone/index.php/Home/Mobile/");
 					HttpResponse httpResponse = httpClient.execute(httpGet);
 					if (httpResponse.getStatusLine().getStatusCode() == 200) {
 						HttpEntity entity = httpResponse.getEntity();
-						String response = EntityUtils.toString(entity, "utf-8");						
+						String response = EntityUtils.toString(entity, "utf-8");
 						Message message = new Message();
 						message.what = 1;
 						message.obj = response.toString();
@@ -139,58 +149,61 @@ public class tapTwo extends Fragment {
 			}
 		}).start();
 	}
+
 	private void parseJSONWithJSONObject(String jsonData) {
 		try {
 			JSONArray jsonArray = new JSONArray(jsonData);
-			Map<String, Object> map ;
+			Map<String, Object> map;
 			for (int i = 0; i < jsonArray.length(); i++) {
 				JSONObject jsonObject = jsonArray.getJSONObject(i);
-				 map = new HashMap<String, Object>();
+				map = new HashMap<String, Object>();
 				String id = jsonObject.getString("gid");
 				String name = jsonObject.getString("price");
-				//String version = jsonObject.getString("original_price");
+				// String version = jsonObject.getString("original_price");
 				map.put("titleTextView", id);
-				map.put("descTextView", name);				
+				map.put("descTextView", name);
 				mListItem.add(map);
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-		public final class ViewHolder {
-			TextView titleTextView;
-			TextView descTextView;
-			ImageView imageView_delete;
-			TextView list_buy_number;
-			ImageView imageView_add;
+
+	// ListView适配器进行设置，加载数据，使用ViewHolder进行优化
+	public final class ViewHolder {
+		TextView titleTextView;
+		TextView descTextView;
+		ImageView imageView_delete;
+		TextView list_buy_number;
+		ImageView imageView_add;
+	}
+
+	public class MyAdapter extends BaseAdapter {
+
+		private LayoutInflater mInflater;
+
+		public MyAdapter(Context context) {
+			this.mInflater = LayoutInflater.from(context);
 		}
 
-		public class MyAdapter extends BaseAdapter {
+		@Override
+		public int getCount() {
+			return mListItem.size();
+		}
 
-			private LayoutInflater mInflater;
+		@Override
+		public Object getItem(int position) {
+			return mListItem.get(position);
+		}
 
-			public MyAdapter(Context context) {
+		@Override
+		public long getItemId(int position) {
+			return 0;
+		}
 
-				this.mInflater = LayoutInflater.from(context);
-			}
-
-			@Override
-			public int getCount() {
-				return mListItem.size();
-			}
-
-			@Override
-			public Object getItem(int position) {
-				return mListItem.get(position);
-			}
-
-			@Override
-			public long getItemId(int position) {
-				return 0;
-			}			
-			@SuppressLint("InflateParams") @Override
+		@SuppressLint("InflateParams") @Override
 			public View getView(int position, View convertView, ViewGroup parent) {
-				Map<String, Object> item = new HashMap<String, Object>();
+				 Map<String, Object> item = new HashMap<String, Object>();
 			    item = mListItem.get(position);//获得所有数据
 				ViewHolder holder = null;
 				if (convertView == null) {
@@ -218,13 +231,14 @@ public class tapTwo extends Fragment {
 				holder.descTextView.setText(item.get("descTextView").toString());
 				
 				//点击添加和减少商品的图标
+				  		        
 				
 				holder.imageView_delete.setOnClickListener(new View.OnClickListener() {
 
 					@Override
 					public void onClick(View v) {
 						// TODO Auto-generated method stub
-						Toast.makeText(getActivity(), "delete user", Toast.LENGTH_SHORT).show();
+						Toast.makeText(getActivity(), "reduce number", Toast.LENGTH_SHORT).show();  
 						
 					}
 
@@ -233,13 +247,26 @@ public class tapTwo extends Fragment {
 
 					@Override
 					public void onClick(View v) {
-						// TODO Auto-generated method stub
-						Toast.makeText(getActivity(), "add user", Toast.LENGTH_SHORT).show();
+						// TODO Auto-generated method stub						
+						Toast.makeText(getActivity(), "add  number", Toast.LENGTH_SHORT).show();
+					    int location=mListView.getCheckedItemPosition();
+					    String l=String.valueOf(location+2);
+						Log.d("position", l);
+						for(int i=0;i<mListView.getChildCount();i++){
+							//LinearLayout layout=(LinearLayout)mListView.getChildAt(i);
+							
+						
+							RelativeLayout layout=(RelativeLayout)mListView.getChildAt(location+1);
+							// 从layout中获得控件,根据其id
+							TextView et = (TextView) layout.findViewById(R.id.list_buy_number);
+						    et.setText(et.getText().toString()+1);
+						
+							}
+						
+						//count.setText(10);
 					}
-
 				});
 				return convertView;			
 			}
 	}
-		}
-	
+}
